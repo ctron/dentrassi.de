8a312caf26677f06c87a859d1cdff2e9ed47eacf
Jens Reimann <ctron@dentrassi.de>
2023-09-10 19:56:37 +0200

    working version of sticky

--- themes/dentrassi/build/build.sh
+++ themes/dentrassi/build/build.sh
@@ -3,5 +3,7 @@
 set -e
 
 rsync -r node_modules/@fontsource/jost/files/ ../static/files/
+mkdir -p ../static/js/
+cp node_modules/bootstrap/dist/js/bootstrap.bundle.min.js ../static/js/
 
 # we don't copy the icons, we embed the SVG
